5d0149dc1621e156a3939958abc96432c96ad7db
Lina Maria Ortiz P <linamaria.ortizparra@studio.unibo.it>
2024-10-11 15:38:10 -0500

    initial commit

--- app.py
+++ app.py
@@ -1,30 +1,27 @@
 import streamlit as st
-import pandas as pd
 import scipy.stats
 import time
+import pandas as pd
 
-# estas son variables de estado que se conservan cuando Streamlin vuelve a ejecutar este script
 if 'experiment_no' not in st.session_state:
-    st.session_state['experiment_no'] = 0
+    st.session_state['experiment_no']=0
 
 if 'df_experiment_results' not in st.session_state:
-    st.session_state['df_experiment_results'] = pd.DataFrame(columns=['no', 'iteraciones', 'media'])
+    st.session_state['df_experiment_results']=pd.DataFrame(columns=['no','iteraciones','media'])
 
 st.header('Lanzar una moneda')
 
-chart=st.line_chart([0.5])
+chart = st.line_chart([0.5])
 
-def toss_coin(n): # función que emula el lanzamiento de una moneda
-
-    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5, size=n)
-
-    mean = None
+def toss_coin(n):
+    trial_outcomes = scipy.stats.bernoulli.rvs(p=0.5,size=n)
+    mean=None
     outcome_no = 0
-    outcome_1_count = 0
+    outcome_1_count=0
 
     for r in trial_outcomes:
         outcome_no +=1
-        if r == 1:
+        if r==1:
             outcome_1_count += 1
         mean = outcome_1_count / outcome_no
         chart.add_rows([mean])
@@ -32,22 +29,14 @@ def toss_coin(n): # función que emula el lanzamiento de una moneda
 
     return mean
 
-number_of_trials = st.slider('¿Número de intentos?',1,1000,10)
-start_button= st.button('Ejecutar')
+number_of_trials = st.slider('¿Número de intentos?', 1, 1000, 10)
+start_button = st.button('Ejecutar')
 
 if start_button:
     st.write(f'Experimento con {number_of_trials} intentos en curso.')
-    st.session_state['experimento_no']+=1
-    mean=toss_coin(number_of_trials)
-    st.session_state['df_experiment_results'] = pd.concat([
-        st.session_state['df_experiment_results'],
-        pd.DataFrame(data=[[st.session_state['experiment_no'],
-                            number_of_trials,
-                            mean]],
-                     columns=['no', 'iterations', 'mean'])
-        ],
-        axis=0)
-    st.session_state['df_experiment_results'] =
-        st.session_state['df_experiment_results'].reset_index(drop=True)
+    st.session_state['experiment_no']+=1
+    mean = toss_coin(number_of_trials)
+    st.session_state['df_experiment_results'] = pd.concat([st.session_state['df_experiment_results'],pd.DataFrame(data=[[st.session_state['experiment_no'],number_of_trials,mean]],columns=['no', 'iterations', 'mean'])],axis=0)
+    st.session_state['df_experiment_results']=st.session_state['df_experiment_results'].reset_index(drop=True)
 
 st.write(st.session_state['df_experiment_results'])
